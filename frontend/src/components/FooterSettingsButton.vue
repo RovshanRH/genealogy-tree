709773b6111ebb75cf1d04f1fc23685e1e77922f
RovshanRH <rovshanbro@gmail.com>
2026-05-09 21:07:28 +0300

    feat: added SIMPLIEST vue components to start with smth

---
+++ frontend/src/components/FooterSettingsButton.vue
@@ -0,0 +1,16 @@
+<!-- Кнопка настроек в футере -->
+
+<template>
+  <button class="with-icon-btn" @click="$emit('clicked')">
+    <slot name="icon"></slot>
+    <span>{{ label }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+    props: {
+        label: String
+    }
+}
+</script>
