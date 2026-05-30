1340b85d322afc17acb8d30e2fdeb71997db5f39
Rovian <rovshanbro@gmail.com>
2026-05-31 00:51:08 +0300

    making resolvers good and nesty with prisma

--- backend/api/resolvers.ts
+++ backend/api/resolvers.ts
@@ -4,23 +4,127 @@ import { z } from "zod";
 import { publicProcedure } from "../api_auth/trpc.ts";
 // import { publicProcedure } from '../api_auth/trpc';
 import { db } from "./server.ts";
-import type { personFindFirstArgs } from "../generated/prisma/models/person";
+import type {
+  personCreateInput,
+  personFindFirstArgs,
+  personUpdateInput,
+} from "../generated/prisma/models/person";
+import type { apartmentCreateWithoutPersonInput } from "../generated/prisma/models/apartment.ts";
+import type { houseCreateWithoutApartmentInput } from "../generated/prisma/models/house.ts";
+import type { streetCreateWithoutHouseInput } from "../generated/prisma/models/street.ts";
+import type {
+  cityCreateWithoutStreetInput,
+  cityCreateOrConnectWithoutRegionInput,
+} from "../generated/prisma/models/city.ts";
+import type { regionCreateWithoutCityInput } from "../generated/prisma/models/region.ts";
+import type { countryCreateWithoutRegionInput } from "../generated/prisma/models/country.ts";
 import { AnyNull } from "../generated/prisma/internal/prismaNamespace";
 // import { prisma } from '../lib/prisma';
+// import { Context } from '../server/context';
+import type { PrismaClient } from "@prisma/client/extension";
+// import { apartment } from '../generated/prisma/browser';
+
 
 export const resolvers = {
   Query: {
     trees: async () => await prisma?.geneology_tree.findMany(),
-    tree: async (_parent: unknown, _args: { id: string }) =>
+    tree: async (_parent: unknown, args: { id: string }) =>
       await prisma?.geneology_tree.findFirst({
-        where: { id: _args.id },
+        where: { id: args.id },
       }),
     persons: async () => await prisma?.person.findMany(),
-    person: async (_parent: unknown, _args: { id: string }) =>
+    person: async (_parent: unknown, args: { id: string }) =>
       await prisma?.person.findFirst({
-        where: { id: _args.id },
+        where: { id: args.id },
       }),
-
   },
-  Mutation: {},
+  // resolvers.ts
+  Mutation: {
+    createPerson: async (
+      _parent: unknown,
+      args: {
+        input: personCreateInput;
+        apartment_input: apartmentCreateWithoutPersonInput;
+        house_input: houseCreateWithoutApartmentInput;
+        street_input: streetCreateWithoutHouseInput;
+        city_input: cityCreateWithoutStreetInput;
+        region_input: regionCreateWithoutCityInput;
+        country_input: countryCreateWithoutRegionInput;
+        city_where: cityCreateOrConnectWithoutRegionInput;
+      },
+      context: { prisma: PrismaClient },
+    ) => {
+      try {
+        const { input } = args;
+        const { prisma } = context;
+
+        const newPerson = prisma?.person.create({
+          data: {
+            first_name: input.first_name,
+            patronymic: input.patronymic,
+            gender: input.gender,
+            birth_date: input.birth_date,
+            birth_date_approx: input.birth_date_approx,
+            death_date: input.death_date,
+            death_date_approx: input.death_date_approx,
+            bio: input.bio,
+            source_info: input.source_info,
+            is_person_contacted: input.is_person_contacted,
+            created_at: new Date(),
+            updated_at: new Date(),
+            // связанные строки
+            // apartment: {
+            //   create: {
+            //     name: args.apartment_input.name,
+            //     created_at: new Date(),
+            //     house: {
+            //       create: {
+            //         created_at: new Date(),
+            //         name: args.house_input.name,
+            //         street: {
+            //           create: {
+            //             created_at: new Date(),
+            //             name: args.street_input.name,
+            //             city: {
+            //               created_at: new Date(),
+            //               name: args.city_input.name,
+            //               region: {
+            //                 create: {
+            //                   created_at: new Date(),
+            //                   name: args.region_input.name,
+            //                   country: {
+            //                     create: {
+            //                       created_at: new Date(),
+            //                       name: args.country_input.name,
+            //                     },
+            //                   },
+            //                 },
+            //               },
+            //             },
+            //           },
+            //         },
+            //       },
+            //     },
+            //   },
+            // },
+          },
+        });
+        return newPerson;
+      } catch (error: any) {
+        console.error("Error creating person:", error);
+        throw new Error(`Failed to create person: ${error.message}`);
+      }
+    },
+    updatePerson: async (
+      _parent: unknown,
+      args: { input: personUpdateInput },
+    ) => {
+      try {
+        const { input } = args;
+      } catch (error: any) {
+        console.error("Error creating person:", error);
+        throw new Error(`Failed to create person: ${error.message}`);
+      }
+    },
+  },
 };
--- backend/api/server.ts
+++ backend/api/server.ts
@@ -1,210 +1,41 @@
 import pkg from "pg";
-import { dbConfig } from "./dbConfig";
+import { dbConfig } from "../dbConfig";
 // import {nanoid} from "nanoid"
 // const bcrypt = require('bcrypt');
-import bcrypt from "bcrypt";
-const { Pool } = pkg;
+// import bcrypt from "bcrypt";
+import { prisma } from '../lib/prisma.ts';
+// const { Pool } = pkg;
 
-const pool = new Pool({
-  user: dbConfig.user,
-  password: dbConfig.password,
-  host: dbConfig.host,
-  port: dbConfig.port,
-  database: dbConfig.database_name,
-});
+// type person = prisma.person;
 
-interface Geneology_tree {
-  id: string,
-  name: string;
-}
-
-interface Person {
-  id: string,
-  geneology_tree_id: string;
-  // surname: string,
-  // maiden_surname: string,
-  first_name: string;
-  patronymic: string;
-  gender: "male" | "female";
-  // birth_date: string,
-  // birth_date_approx: boolean,
-  // birth_place_country_id: string,
-  // birth_place_city_id: string,
-  // birth_place_street: string,
-  // birth_place_house: string,
-  // birth_place_apartment: string,
-  bio: string;
-  source_info: string;
-  is_person_contacted: boolean;
-}
-
-
-// password hashing
-async function hashPassword(plainPassword: string) {
-  const saltRounds = 12;
-  const hashedPassword = await bcrypt.hash(plainPassword, saltRounds);
-  return hashedPassword;
-}
-
-async function verifyPassword(
-  plainPassword: string,
-  hashedPasswordFromDB: string,
-) {
-  const isValid = await bcrypt.compare(plainPassword, hashedPasswordFromDB);
-  return isValid;
+// async function createPerson(input: prisma.person) : prisma.person => {
+//   return await 0;
+// }
+  
+async function name(params:string) {
+  
 }
 
 export const db = {
-  // User methods
-  findUserByID: async (id: string) => {
-    const result = await pool.query(
-      `SELECT * FROM Users where id=$1 and role=1;`,
-      [id],
-    );
-    return result.rows[0];
-  },
-  // findAdminByID: async (id: Number) => {
-  //     const result = await pool.query(`SELECT * FROM Users where id=$1 and role=2`, [id]);
-  //     return result.rows[0];
-  // },
-  createUser: async (
-    username: string,
-    email: string,
-    password: string,
-    role: Number,
-    is_blocked: Boolean,
-  ) => {
-    const hash_password = hashPassword(password);
-    await pool.query(
-      `INSERT INTO Users (username, email, password_hash, role_id, is_blocked) VALUES
-            (
-                $1,
-                $2,
-                $3,
-                $4,
-                $5
-            );
-
-            `,
-      [username, email, hashPassword, role, is_blocked],
-    );
-  },
-  findUsers: async () => {
-    const result = await pool.query(`SELECT * from Users;`);
-    return result.rows;
-  },
-  // Tree methods
-
-  findTreeByID: async (id: string) => {
-    const result = await pool.query(
-      `SELECT * from geneology_tree where id=$1;`,
-      [id],
-    );
-    return result.rows[0];
-  },
-  findTrees: async () => {
-    const result = await pool.query(`SELECT * from geneology_tree;`);
-    return result.rows;
-  },
-
-  createGeneologyTree: async (input: Geneology_tree) => {
-    await pool.query(
-      `INSERT INTO geneology_tree name VALUES
-            (
-                $1
-            );
-            `,
-      [
-        // input.id,
-        input.name,
-      ],
-    );
-  },
-  updateGeneologyTree: async (input: Geneology_tree, id: string) => {
-    await pool.query(`UPDATE geneology_tree SET name=$1 where id=$2;`,
-        [input.name , id]
+  // createPerson: async () => {
+  //     prisma?.person.create
+  // }
+  findPersonById: async (id: string) => {
+    prisma?.person.findFirst(
+      {
+        where: {
+          id: id
+        }
+      }
     )
   },
-  deleteGeneologyTree: async (id: string) => {
-    const result = await pool.query(`DELETE FROM geneology_tree where id=$1;`, [id]);
-    return result.rows.length > 0;
-  },
-
-  // person methods
-
-  findPersonByID: async (id: string) => {
-    const result = await pool.query(`SELECT * from person WHERE id=$1;`, [id]);
-    return result.rows[0];
-  },
-
-  findPersons: async () => {
-    const result = await pool.query(`SELECT * FROM person;`);
-    return result.rows;
-  },
-
-  createPerson: async (person: Person) => {
-    await pool.query(
-      `INSERT INTO person VALUES
-            (
-                $1,
-                $2,
-                $3,
-                $4,
-                $5,
-                $6,
-                $7,
-
-
-            )
-                where id=$8;
-            `,
-      [
-        person.geneology_tree_id,
-        person.first_name,
-        person.patronymic,
-        person.gender,
-        person.bio,
-        person.source_info,
-        person.is_person_contacted,
-        person.id
-      ],
-    );
-  },
-  updatePerson: async (person: Person) => {
-    await pool.query(
-      `UPDATE person SET
-            
-        geneology_tree_id=$1,
-        first_name=$2,
-        patronymic=$3,
-        gender=$4,
-        bio=$5,
-        source_info=$6,
-        is_person_contacted=$7
-
-        where id=$8;
-
-
-            
-            `,
-      [
-        person.geneology_tree_id,
-        person.first_name,
-        person.patronymic,
-        person.gender,
-        person.bio,
-        person.source_info,
-        person.is_person_contacted,
-        person.id
-      ],
-    );
-  },
-  deletePerson: async (id: string) => {
-    const result = await pool.query(`DELETE FROM geneology_tree where id=$1;`,
-      [id]
-    );
-    return result.rows.length > 0;
+  findTreeById: async (id:string) => {
+      prisma?.geneology_tree.findFirst({
+        where: {
+          id: id
+        }
+      })
   }
-  
+}
 
-};
+
